524d6c99c68421fe3d4e815b579d1b6361626afa
SamirPS <akariohsamir@yahoo.com>
2022-11-05 18:35:42 +0100

    add tree

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,4 +2,11 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
   use "EdenEast/nightfox.nvim"
+  use {
+    'nvim-tree/nvim-tree.lua',
+    requires = {
+      'nvim-tree/nvim-web-devicons', -- optional, for file icons
+    },
+    tag = 'nightly' -- optional, updated every week. (see issue #1193)
+  }
 end)
