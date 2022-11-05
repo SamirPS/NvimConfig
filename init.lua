5cf0036a25c41b37fba0ea409c55c7c51099f2e7
SamirPS <akariohsamir@yahoo.com>
2022-11-05 19:46:14 +0100

    indent blackline

--- init.lua
+++ init.lua
@@ -44,3 +44,11 @@ require('bufferline').setup({
       }
     }
   })
+
+  require('indent_blankline').setup({
+    char = '▏',
+    show_trailing_blankline_indent = false,
+    show_first_indent_level = false,
+    use_treesitter = true,
+    show_current_context = false
+  })
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -14,4 +14,6 @@ return require('packer').startup(function(use)
     requires = { 'kyazdani42/nvim-web-devicons', opt = true }
   }
   use {'akinsho/bufferline.nvim', tag = "v3.*", requires = 'kyazdani42/nvim-web-devicons'}
+  use "lukas-reineke/indent-blankline.nvim"
+
 end)
